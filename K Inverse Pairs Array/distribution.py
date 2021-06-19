ad0e5ab0cc08586e95aa3eaea664e0f5d20d7134
Raman <65412379+rmnsnh@users.noreply.github.com>
2021-06-19 15:49:15 -0400

    research for inverse pairs
    k inverse pairs research

---
+++ K Inverse Pairs Array/distribution.py
@@ -0,0 +1,29 @@
+from typing import List
+from itertools import permutations
+from collections import Counter
+
+class Solution:
+    def count(l):
+        ret = 0
+        for i, num in enumerate(l):
+            for x in range(i + 1, len(l)):
+                if num > l[x]:
+                    ret += 1
+        return ret
+
+    def kInversePairs(self, n: int, k: int) -> None:
+        a = list(permutations(range(1, n)))
+        result = list(map(Solution.count, a))
+        print(Counter(result))
+        print(len(Counter(result).keys()))
+        # print(a)
+        print(result)
+
+s = Solution()
+s.kInversePairs(1, 1)
+s.kInversePairs(2, 1)
+s.kInversePairs(3, 1)
+s.kInversePairs(4, 1)
+s.kInversePairs(5, 1)
+s.kInversePairs(6, 1)
+s.kInversePairs(7, 1)
