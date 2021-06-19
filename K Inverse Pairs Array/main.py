1659902448edb13ece6ab60024ae19322a61ce91
Raman <65412379+rmnsnh@users.noreply.github.com>
2021-06-19 19:55:18 -0400

    k inverse v0.5
    kinverse done for now, not sure of implementaiton

--- K Inverse Pairs Array/main.py
+++ K Inverse Pairs Array/main.py
@@ -1,1 +1,26 @@
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        return Solution.recurse(n, k)
 
+        # if (dp[n][k] != -1) return dp[n][k];
+        # if (k == 0) return dp[n][k] = 1;
+        # if (n == 0) return dp[n][k] = 0;
+        # int j = 0, val = 0;
+        # for (j = 0; j < n && k-j >= 0; j++)
+        #     val += inversions(n-1, k-j);
+        # return dp[n][k] = val;
+    def recurse(n, k):
+        if k == 0:
+            return 0
+        if n == 0:
+            return 0
+        ret = 0
+        i = 0
+        while i < n and k-i >= 0:
+            ret += Solution.recurse(n-1, k-i)
+            i += 1
+        return ret
+
+s = Solution()
+print(s.kInversePairs(3, 0))
+print(s.kInversePairs(3, 1))
