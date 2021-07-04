570936feed45e0e3805b2019ca81ebff8f3b631a
Raman <65412379+rmnsnh@users.noreply.github.com>
2021-07-04 19:01:20 -0400

    Update main.py
    run times dont make sense ok

--- Count Vowels Permutation/main.py
+++ Count Vowels Permutation/main.py
@@ -3,12 +3,9 @@ from typing import List
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
         count = [1] * 5
-        next = [0] * 5
         while n != 1:
-            next = [count[1] + count[2] + count[4], count[0] + count[2], count[1] + count[3], count[2], count[2] + count[3]]
+            count = [count[1] + count[2] + count[4], count[0] + count[2], count[1] + count[3], count[2], count[2] + count[3]]
             n -= 1
-            count = next
-            next = [0] * 5
         return sum(count) % 1000000007
 
 s = Solution()
