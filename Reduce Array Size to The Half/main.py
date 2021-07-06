6a78b7b3dbfab37a9017503729a69e9ec0cfec09
Raman <65412379+rmnsnh@users.noreply.github.com>
2021-07-06 15:06:54 -0400

    Update main.py
    fin

--- Reduce Array Size to The Half/main.py
+++ Reduce Array Size to The Half/main.py
@@ -14,18 +14,15 @@ from collections import Counter
 
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        l = len(arr)
-        c = Counter(arr).most_common()
-        i = 0
-        size = 0
-        while size < (l // 2):
-            size += c[i][1]
-            i += 1
-        return i
-
-
-                c=sort(list(Counter(arr).values())
+        n=len(arr)
+        k=list(Counter(arr).values())
         k.sort(reverse=True)
+        ans=0
+        c=0
+        while ans<n//2:
+            ans+=k[c]
+            c+=1
+        return c
 
 s = Solution()
 print(s.minSetSize([3,3,3,3,5,5,5,2,2,7]))
